617b6c6ae4162d36c6b870f70d5eb7596259310f
Saloni <116108392+saloni20404@users.noreply.github.com>
2026-04-06 22:46:58 +0530

    fix: save user to mongodb on signin

--- auth.ts
+++ auth.ts
@@ -1,5 +1,7 @@
 import NextAuth from 'next-auth'
 import GitHub from 'next-auth/providers/github'
+import { connectToDatabase } from '@/lib/mongodb'
+import User from '@/lib/models/User'
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   trustHost: true,
@@ -7,7 +9,43 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     GitHub({
       clientId: process.env.GITHUB_CLIENT_ID!,
       clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+      authorization: { params: { scope: 'repo user' } },
     }),
   ],
+  callbacks: {
+    async signIn({ user, account }) {
+      try {
+        if (account?.provider === 'github' && account.access_token) {
+          await connectToDatabase()
+          await User.findOneAndUpdate(
+            { githubId: account.providerAccountId },
+            {
+              name: user.name,
+              email: user.email,
+              image: user.image,
+              githubId: account.providerAccountId,
+              githubAccessToken: account.access_token,
+            },
+            { upsert: true, new: true }
+          )
+        }
+        return true
+      } catch (error) {
+        console.error('SignIn error:', error)
+        return true
+      }
+    },
+    async jwt({ token, account }) {
+      if (account) {
+        token.githubId = account.providerAccountId
+        token.accessToken = account.access_token
+      }
+      return token
+    },
+    async session({ session, token }) {
+      session.user.id = token.githubId as string
+      return session
+    },
+  },
   secret: process.env.AUTH_SECRET,
 })
